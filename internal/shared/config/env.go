ce39b11c4e11a4896ea5903bf519cc629d67a311
Agif Sofyan <alpenizme@gmail.com>
2026-08-05 16:28:13 +0700

    change env declaration

--- internal/shared/config/env.go
+++ internal/shared/config/env.go
@@ -3,10 +3,10 @@ package config
 func overrideEnv(cfg *Config) {
 	cfg.App.Env = getenv("APP_ENV", cfg.App.Env)
 	cfg.Server.Port = getenvInt("SERVER_PORT", cfg.Server.Port)
-	cfg.Database.Host = getenv("DB_HOST", cfg.Database.Host)
-	cfg.Database.Port = getenvInt("DB_PORT", cfg.Database.Port)
-	cfg.Database.User = getenv("DB_USER", cfg.Database.User)
-	cfg.Database.Password = getenv("DB_PASSWORD", cfg.Database.Password)
-	cfg.Database.Name = getenv("DB_NAME", cfg.Database.Name)
+	cfg.Database.Host = getenv("DATABASE_HOST", cfg.Database.Host)
+	cfg.Database.Port = getenvInt("DATABASE_PORT", cfg.Database.Port)
+	cfg.Database.User = getenv("DATABASE_USER", cfg.Database.User)
+	cfg.Database.Password = getenv("DATABASE_PASSWORD", cfg.Database.Password)
+	cfg.Database.Name = getenv("DATABASE_NAME", cfg.Database.Name)
 	cfg.Logger.Level = getenv("LOGGER_LEVEL", cfg.Logger.Level)
 }
